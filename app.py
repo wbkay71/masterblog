e1babdd5b46b3c02ef395c2c10383415687ffee1
wbkay71 <wanja.kneib@googlemail.com>
2025-08-01 11:58:07 +0200

    feat: Add blog posts data structure and index route
    - Create blog_posts.json with sample data
    - Add load_blog_posts function to read from JSON
    - Update index route to display all posts
    - Change port to 5001 to avoid conflicts

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 
+def load_blog_posts():
+    """Load blog posts from JSON file"""
+    with open('blog_posts.json', 'r') as file:
+        blog_posts = json.load(file)
+    return blog_posts
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    # Load blog posts from JSON file
+    blog_posts = load_blog_posts()
+
+    # Render template with blog posts
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
