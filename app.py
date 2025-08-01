5c9b06a3d3c2502f9649f5ff2a7feeac857bc47d
wbkay71 <wanja.kneib@googlemail.com>
2025-08-01 12:38:28 +0200

    feat: Add create new blog post functionality
    - Add /add route with GET and POST handling
    - Create add.html template with form for new posts
    - Implement save_blog_posts function to write to JSON
    - Add ID generation for new posts
    - Add navigation link from home page to add post page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 app = Flask(__name__)
@@ -11,6 +11,12 @@ def load_blog_posts():
     return blog_posts
 
 
+def save_blog_posts(posts):
+    """Save blog posts to JSON file"""
+    with open('blog_posts.json', 'w') as file:
+        json.dump(posts, file, indent=4)
+
+
 @app.route('/')
 def index():
     # Load blog posts from JSON file
@@ -18,6 +24,39 @@ def index():
 
     # Render template with blog posts
     return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        # Load existing posts
+        blog_posts = load_blog_posts()
+
+        # Generate new ID (highest ID + 1)
+        if blog_posts:
+            new_id = max(post['id'] for post in blog_posts) + 1
+        else:
+            new_id = 1
+
+        # Create new post from form data
+        new_post = {
+            'id': new_id,
+            'author': request.form.get('author'),
+            'title': request.form.get('title'),
+            'content': request.form.get('content')
+        }
+
+        # Add new post to list
+        blog_posts.append(new_post)
+
+        # Save updated posts to JSON file
+        save_blog_posts(blog_posts)
+
+        # Redirect to home page
+        return redirect(url_for('index'))
+
+    # If GET request, show the form
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
